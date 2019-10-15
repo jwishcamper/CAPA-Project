f671ddf9e00ad7d20b4b8534b2e6bbb211e5cff6
Jonathan Wishcamper <j.wishcamper@bellevuecollege.edu>
2019-10-14 23:17:02 -0700

    Initial version

---
+++ app/src/main/java/com/example/capaproject/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.capaproject
+
+import android.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val fragHeight = 350
+const val paddingHeight = 35
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        for (i in 1..7){ createFragment("testFragment") }
+    }
+
+    //creates a new frame and fragment in it of type fragmentType
+    private fun createFragment(fragmentType:String){
+        //add padding
+        var newPadding = FrameLayout(this)
+        linearLayout.addView(newPadding)
+        newPadding.layoutParams.height = paddingHeight
+        newPadding.layoutParams.width =  ActionBar.LayoutParams.MATCH_PARENT
+
+        //add new frame
+        var newFrag = FrameLayout(this)
+        newFrag.id = ViewCompat.generateViewId()
+        linearLayout.addView(newFrag)
+        newFrag.layoutParams.height = fragHeight
+        newFrag.layoutParams.width =  ActionBar.LayoutParams.MATCH_PARENT
+
+        //add fragment to created frame
+        when(fragmentType) {
+            "testFragment" -> addFragment(testingFragment(), newFrag.id)
+        }
+    }
+
+
+    //helper functions to add fragments more easily
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+    fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int){
+        supportFragmentManager.inTransaction { add(frameId, fragment) }
+    }
+    fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
+        supportFragmentManager.inTransaction{replace(frameId, fragment)}
+    }
+}
---
+++ app/src/main/java/com/example/capaproject/testingFragment.kt
@@ -0,0 +1,26 @@
+package com.example.capaproject
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class testingFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_testing, container, false)
+    }
+
+
+
+}

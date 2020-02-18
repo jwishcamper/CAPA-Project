e3604a74687ecccff80f64a11afec806b41a513a
Nick Brouwer <44852318+nebrouwer@users.noreply.github.com>
2020-02-18 12:46:28 -0800

    Saving user history

--- app/src/main/java/com/example/capaproject/DatabaseHandler.kt
+++ app/src/main/java/com/example/capaproject/DatabaseHandler.kt
@@ -6,8 +6,11 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
+import android.util.Log
 import com.example.capaproject.SurveyReaderContract.SurveyEntry
 import com.example.capaproject.WorkReaderContract.WorkEntry
+import com.example.capaproject.DefaultReaderContract.DefaultEntry
+import com.example.capaproject.SchoolReaderContract.SchoolEntry
 import com.example.capaproject.UserPrefsContract.UserPrefsEntry
 import com.example.capaproject.UserDataContract.UserDataEntry
 
@@ -26,8 +29,8 @@ private object SurveyReaderContract{
 
 private object UserDataContract{
     object UserDataEntry : BaseColumns{
-        const val TABLE_NAME = "User Data"
-        const val COLUMN_DATE_TIME = "Date & Time"
+        const val TABLE_NAME = "UserData"
+        const val COLUMN_DATE_TIME = "DateTime"
         const val COLUMN_STATE = "State"
         const val COLUMN_LATITUDE = "Latitude"
         const val COLUMN_LONGITUDE = "Longitude"
@@ -37,6 +40,24 @@ private object UserDataContract{
 private object WorkReaderContract{
     object WorkEntry : BaseColumns{
         const val TABLE_NAME = "Work"
+        const val COLUMN_PACKAGE = "Package"
+        const val COLUMN_CLASS = "Class"
+        const val COLUMN_WEIGHT = "Weight"
+    }
+}
+
+private object DefaultReaderContract{
+    object DefaultEntry : BaseColumns{
+        const val TABLE_NAME = "DefaultTable"
+        const val COLUMN_PACKAGE = "Package"
+        const val COLUMN_CLASS = "Class"
+        const val COLUMN_WEIGHT = "Weight"
+    }
+}
+
+private object SchoolReaderContract{
+    object SchoolEntry : BaseColumns{
+        const val TABLE_NAME = "School"
         const val COLUMN_PACKAGE = "Package"
         const val COLUMN_CLASS = "Class"
         const val COLUMN_WEIGHT = "Weight"
@@ -72,16 +93,32 @@ private const val WORK_CREATE_ENTRIES =
             "${WorkEntry.COLUMN_CLASS} TEXT," +
             "${WorkEntry.COLUMN_WEIGHT} DOUBLE)"
 
+private const val DEFAULT_CREATE_ENTRIES =
+    "CREATE TABLE IF NOT EXISTS ${DefaultEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${DefaultEntry.COLUMN_PACKAGE} TEXT," +
+            "${DefaultEntry.COLUMN_CLASS} TEXT," +
+            "${DefaultEntry.COLUMN_WEIGHT} DOUBLE)"
+
+private const val SCHOOL_CREATE_ENTRIES =
+    "CREATE TABLE IF NOT EXISTS ${SchoolEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${SchoolEntry.COLUMN_PACKAGE} TEXT," +
+            "${SchoolEntry.COLUMN_CLASS} TEXT," +
+            "${SchoolEntry.COLUMN_WEIGHT} DOUBLE)"
+
 private const val USER_DATA_CREATE_ENTRIES =
     "CREATE TABLE IF NOT EXISTS ${UserDataEntry.TABLE_NAME} (" +
             "${BaseColumns._ID} INTEGER PRIMARY KEY," +
             "${UserDataEntry.COLUMN_DATE_TIME} TEXT," +
-            "${UserDataEntry.COLUMN_STATE} TEXT)," +
+            "${UserDataEntry.COLUMN_STATE} TEXT," +
             "${UserDataEntry.COLUMN_LATITUDE} DOUBLE," +
-            "${UserDataEntry.COLUMN_LONGITUDE} DOUBLE"
+            "${UserDataEntry.COLUMN_LONGITUDE} DOUBLE)"
 
 private const val SURVEY_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${SurveyEntry.TABLE_NAME}"
 private const val WORK_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${WorkEntry.TABLE_NAME}"
+private const val DEFAULT_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${DefaultEntry.TABLE_NAME}"
+private const val SCHOOL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${SchoolEntry.TABLE_NAME}"
 private const val USER_PREFS_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${UserPrefsEntry.TABLE_NAME}"
 private const val USER_DATA_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${UserDataEntry.TABLE_NAME}"
 
@@ -91,13 +128,17 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         db.execSQL(WORK_CREATE_ENTRIES)
         db.execSQL(USER_PREFS_CREATE_ENTRIES)
         db.execSQL(USER_DATA_CREATE_ENTRIES)
+        db.execSQL(SCHOOL_CREATE_ENTRIES)
+        db.execSQL(DEFAULT_CREATE_ENTRIES)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL(SURVEY_DELETE_ENTRIES)
         db.execSQL(WORK_DELETE_ENTRIES)
         db.execSQL(USER_PREFS_DELETE_ENTRIES)
+        db.execSQL(SCHOOL_DELETE_ENTRIES)
         //db.execSQL(USER_DATA_DELETE_ENTRIES)
+        db.execSQL(DEFAULT_DELETE_ENTRIES)
         onCreate(db)
     }
     companion object{
@@ -112,6 +153,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     //Function to update user preferences for every state
     fun updateStatePrefs(prefs : UserPrefApps){
         updateWorkPrefs(prefs)
+        updateSchoolPrefs(prefs)
     }
 
     //Function to update user preferences for Work state
@@ -125,29 +167,70 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         val clockPKG = clock.packageName
         val clockCLS = clock.className
 
+        //var selection = "${WorkEntry.COLUMN_CLASS}=${clockCLS}"
+        db.query(WorkEntry.TABLE_NAME, null, "${WorkEntry.COLUMN_CLASS}=${clockCLS}", null, null, null, null, null)
+
         val clockValues = ContentValues().apply{
             put(WorkEntry.COLUMN_PACKAGE, clockPKG)
             put(WorkEntry.COLUMN_CLASS, clockCLS)
         }
 
-        db.update(WorkEntry.TABLE_NAME, clockValues, "${WorkEntry.COLUMN_CLASS}=$clockCLS", null)
+        db.update(WorkEntry.TABLE_NAME, clockValues, null, null)
 
         val musicPKG = music.packageName
         val musicCLS = music.className
+
+        //selection = "SELECT * FROM ${WorkEntry.TABLE_NAME} WHERE ${WorkEntry.COLUMN_CLASS}=${musicCLS}"
+        db.query(WorkEntry.TABLE_NAME, null, "${WorkEntry.COLUMN_CLASS}=${musicCLS}", null, null, null, null, null)
 
         val musicValues = ContentValues().apply{
             put(WorkEntry.COLUMN_PACKAGE, musicPKG)
             put(WorkEntry.COLUMN_CLASS, musicCLS)
         }
 
-        db.update(WorkEntry.TABLE_NAME, musicValues, "${WorkEntry.COLUMN_CLASS}=$musicCLS", null)
+        db.update(WorkEntry.TABLE_NAME, musicValues, null, null)
 
-        /*val selectQuery = "SELECT * FROM ${WorkEntry.TABLE_NAME}"
+        val selectQuery = "SELECT * FROM ${WorkEntry.TABLE_NAME}"
         val cursor = db.rawQuery(selectQuery, null)
         cursor.moveToFirst()
         while(!cursor.isAfterLast){
+            //Log.d("test", cursor.getString(cursor.getColumnIndex(WorkEntry.COLUMN_CLASS)))
+        }
+    }
 
-        }*/
+    private fun updateSchoolPrefs(prefs: UserPrefApps) {
+        val db = this.writableDatabase
+        db.execSQL(SCHOOL_CREATE_ENTRIES)
+
+        val clock = prefs.getAttr("Clock")
+        val music = prefs.getAttr("Music")
+
+        val clockPKG = clock.packageName
+        val clockCLS = clock.className
+
+        val clockValues = ContentValues().apply{
+            put(SchoolEntry.COLUMN_PACKAGE, clockPKG)
+            put(SchoolEntry.COLUMN_CLASS, clockCLS)
+        }
+
+        db.replace(SchoolEntry.TABLE_NAME, null, clockValues)
+
+        val musicPKG = music.packageName
+        val musicCLS = music.className
+
+        val musicValues = ContentValues().apply{
+            put(SchoolEntry.COLUMN_PACKAGE, musicPKG)
+            put(SchoolEntry.COLUMN_CLASS, musicCLS)
+        }
+
+        db.replace(SchoolEntry.TABLE_NAME, null, musicValues)
+
+        val selectQuery = "SELECT * FROM ${SchoolEntry.TABLE_NAME}"
+        val cursor = db.rawQuery(selectQuery, null)
+        cursor.moveToFirst()
+        while(!cursor.isAfterLast){
+            Log.d("test", cursor.getString(cursor.getColumnIndex(SchoolEntry.COLUMN_CLASS)))
+        }
     }
 
     //Adds or updates user preferences in database
@@ -179,6 +262,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
 
         db.replace(UserPrefsEntry.TABLE_NAME, null, values)
 
+        //updateStatePrefs(prefs)
         //db.close()
     }
 
@@ -238,10 +322,30 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         }
     }
 
+    private fun updateDefaultInfo(map: HashMap<ComponentName, Double>){
+        val db = this.writableDatabase
+        db.execSQL(DEFAULT_DELETE_ENTRIES)
+        db.execSQL(DEFAULT_CREATE_ENTRIES)
+
+        for(entry in map){
+            val pkg = entry.key.packageName
+            val cls = entry.key.className
+            val weight = entry.value
+
+            val values = ContentValues().apply{
+                put(DefaultEntry.COLUMN_PACKAGE, pkg)
+                put(DefaultEntry.COLUMN_CLASS, cls)
+                put(DefaultEntry.COLUMN_WEIGHT, weight)
+            }
+            db.insert(DefaultEntry.TABLE_NAME, null, values)
+        }
+    }
+
     //Updates state info in corresponding table using passed string to check which state
     fun updateDatabaseState(stateName: String, map: HashMap<ComponentName, Double>){
         when(stateName){
-            WORK_TABLE_NAME -> updateWorkInfo(map)
+            "atWork" -> updateWorkInfo(map)
+            "default" -> updateDefaultInfo(map)
         }
     }
 
@@ -275,8 +379,32 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     fun getStateInfo(stateName: String): HashMap<ComponentName, Double>? {
         return when (stateName) {
             "atWork" -> getWorkInfo()
+            "default" -> getDefaultInfo()
             else -> return null
         }
+    }
+
+    private fun getDefaultInfo(): HashMap<ComponentName, Double> {
+        val db = this.readableDatabase
+        db.execSQL(DEFAULT_CREATE_ENTRIES)
+
+        val map: HashMap<ComponentName, Double> = HashMap()
+        val selectQuery = "SELECT * FROM ${DefaultEntry.TABLE_NAME}"
+        val cursor = db.rawQuery(selectQuery, null)
+        cursor!!.moveToFirst()
+        while(!cursor.isAfterLast){
+            val pkg = cursor.getString(cursor.getColumnIndex("Package"))
+            val cls = cursor.getString(cursor.getColumnIndex("Class"))
+            val weight = cursor.getDouble(cursor.getColumnIndex("Weight"))
+            val compName = ComponentName(
+                pkg,
+                cls
+            )
+            map[compName] = weight
+            cursor.moveToNext()
+        }
+        cursor.close()
+        return map
     }
 
     //Gets work state info from database and returns as HashMap
@@ -350,5 +478,14 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
             put(UserDataEntry.COLUMN_LONGITUDE, longitude)
         }
         db.insert(UserDataEntry.TABLE_NAME, null, values)
+
+        val selectQuery = "SELECT * FROM ${UserDataEntry.TABLE_NAME}"
+        val cursor = db.rawQuery(selectQuery, null)
+        cursor.moveToFirst()
+        while(!cursor.isAfterLast){
+            Log.d("test", cursor.getString(cursor.getColumnIndex(UserDataEntry.COLUMN_DATE_TIME)))
+            cursor.moveToNext()
+        }
+        cursor.close()
     }
 }
